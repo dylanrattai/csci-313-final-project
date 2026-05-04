8d08e71b4da350602eb867d43be63bbde1ef89a8
lkittels <lkittels@outlook.com>
2026-05-04 12:11:09 -0500

    Improved profile account info editing

--- src/app/components/profile/profile-content/profile.ts
+++ src/app/components/profile/profile-content/profile.ts
@@ -1,12 +1,18 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { ProfileTab } from '../profile/profile';
+import { Account } from '../tabs/account/account';
+import { AuthService } from '../../../core/services/auth/authService';
 
 @Component({
   selector: 'app-profile-content',
-  imports: [],
+  imports: [Account],
   templateUrl: './profile.html',
   styleUrl: './profile.css',
 })
 export class ProfileContent {
-activeTab = input<ProfileTab>('account');
+  private readonly auth = inject(AuthService);
+  
+  activeTab = input<ProfileTab>('account');
+
+  readonly user = this.auth.appUser;
 }

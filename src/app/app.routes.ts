c232e199a900112697b28d0d19d7236f7396e92f
paige gruber <paige.gruber@ndsu.edu>
2026-05-05 02:04:09 -0500

    added styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,22 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './components/home-page/home-page';
+import { AboutPage } from './components/about-page/about-page';
+import { ContactPage } from './components/contact-page/contact-page';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomePage,
     title: 'Home',
+  },
+  {
+    path:'about',
+    component: AboutPage,
+    title: 'About'
+  },
+    {
+    path:'contact',
+    component: ContactPage,
+    title: 'Contact'
   },
 ];

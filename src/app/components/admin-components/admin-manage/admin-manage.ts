53129f7dafa6807ee297f1e5bfa5dee6b1fe3fa1
dylanrattai <d_rattai@yahoo.com>
2026-05-06 11:38:38 -0500

    Added component and route for admin manage page

---
+++ src/app/components/admin-components/admin-manage/admin-manage.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-manage',
+  imports: [],
+  templateUrl: './admin-manage.html',
+  styleUrl: './admin-manage.css',
+})
+export class AdminManage {
+
+}
